307a7584c118e69bdc92434ada7aca0fcafabee2
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-09-05 12:49:06 +0300

    added file extensions assignment

--- routes/files.js
+++ routes/files.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-var upload = multer({ dest: "uploads/" });
+var multer = require('multer');
+var path = require('path')
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+  }
+})
+
+var upload = multer({ storage: storage });
 
 router.get('/', function(req, res, next) {
   res.send('file');
