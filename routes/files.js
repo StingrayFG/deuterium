967ab7d07df06d245ad2d69350d6dff4b05e652f
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-09-06 12:46:14 +0300

    added uuid generation

--- routes/files.js
+++ routes/files.js
@@ -1,27 +1,35 @@
+var path = require('path');
+var crypto = require('crypto');
+
 var express = require('express');
 var router = express.Router();
 
-var multer = require('multer');
-var path = require('path')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
+var multer = require('multer');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+    cb(null, file.originalname + '.' + Date.now()) //Appending extension
   }
 })
 
 var upload = multer({ storage: storage });
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   res.send('file');
 });
 
-router.post('/', upload.single('file'), function(req, res, next) {
-  console.log('post')
-  res.json({ link: '' });
+router.post('/', upload.single('file'), async function(req, res, next) {
+  var uuid = crypto.randomUUID()
+  
+
+  
+
+  res.json({ link: uuid });
   res.send;
 });
 
