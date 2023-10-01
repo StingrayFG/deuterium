ff670c6193d0184347850bc62f1eaf9bfd8fdd1a
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-01 12:38:16 +0300

    renamed filesRouter

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cors = require('cors')
 
 var indexRouter = require('./routes/index');
-var filesRouter = require('./routes/files');
+var fileRouter = require('./routes/file');
 
 var app = express();
 
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/', filesRouter);
+app.use('/', fileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
